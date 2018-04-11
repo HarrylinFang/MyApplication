0bbf383248315cf407d60c2d4152970a5cbf6c2e
FangJunQi <fjq19931002@163.com>
2018-04-11 16:09:58 +0800

    在代码中改变了textview的文字

--- app/src/main/java/com/howjia/myapplication/MainActivity.java
+++ app/src/main/java/com/howjia/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.howjia.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView tv = (TextView) findViewById(R.id.textView);
+        tv.setText("用户您好，竭诚为您服务！");
     }
     //这里加上一行注释，也作为是一个修改
     //我这里再加上一行注释，测试多次stash
